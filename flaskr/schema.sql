6dbc7885e3c4574de8f8f1f4a10f83dc2c307814
morgan <mwheeler5005@gmail.com>
2019-09-14 11:57:36 -0400

    added database

---
+++ flaskr/schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS event_post;
+DROP TABLE IF EXISTS event_announcement;
+
+CREATE TABLE event_post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT,
+  author_name TEXT NOT NULL,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  event_date TEXT NOT NULL
+);
+
+CREATE TABLE event_announcement (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  author_name TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  event_date NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
