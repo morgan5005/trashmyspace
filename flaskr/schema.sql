dfed368547a1e10e8ecf9895a956eb61a58fdeb6
Robin Brooks <sapricotzine@gmail.com>
2019-09-14 12:31:47 -0400

    added trashoverflow table to db

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS event_post;
 DROP TABLE IF EXISTS event_announcement;
+DROP TABLE IF EXISTS trashoverflow;
 
 CREATE TABLE event_post (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,4 +20,11 @@ CREATE TABLE event_announcement (
   body TEXT NOT NULL,
   event_date NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE trashoverflow (
+    id INTEGER PRIMARY KEY AUDOINCREMENT,
+    location DOUBLE NOT NULL,
+    trashcan_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
